2e8b48914b41f313afff23fb31a7e1ca0b02351d
Miguel A. De Paz <maadxxx@gmail.com>
2020-09-03 00:08:53 -0500

    Update and rename Shotcut-linux-x86_64-200412_Install.sh to Shotcut-linux-x86_64-200711_Install.sh

--- Shotcut-linux-x86_64-200711_Install.sh
+++ Shotcut-linux-x86_64-200711_Install.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-# Ultima actualización: 02/06/2020
+# Ultima actualización: 02/09/2020
 cd /
 # Se descarga Shotcut y logo
-wget https://github.com/mltframework/shotcut/releases/download/v20.04.12/shotcut-linux-x86_64-200412.txz
+wget https://github.com/mltframework/shotcut/releases/download/v20.07.11/shotcut-linux-x86_64-200711.txz
 cd /usr/local/
 echo 'Se va a instalar en /usr/local/...'
 read -p "Ingresa el nombre del directorio principal de instalacion, si no existe se creará: " DIRINST
@@ -20,15 +20,15 @@ if [ -d $DIRINST ];
 		cd $DIRINST
 fi
 sleep 2
-mv /shotcut-linux-x86_64-200412.txz .
-tar xf shotcut-linux-x86_64-200412.txz
+mv /shotcut-linux-x86_64-200711.txz .
+tar xf shotcut-linux-x86_64-200711.txz
 mv Shotcut/ shotcut
 #
 #App a Menú
 cd /usr/share/applications/
 touch shotcut.desktop
 echo '[Desktop Entry]' > shotcut.desktop
-echo 'Version=20.04.12' >> shotcut.desktop
+echo 'Version=20.07.11' >> shotcut.desktop
 echo 'Name=Shotcut' >> shotcut.desktop
 echo 'Comment=Video Editor' >> shotcut.desktop
 echo 'Exec=/usr/local/'$DIRINST'/shotcut/Shotcut.app/shotcut' >> shotcut.desktop
@@ -69,7 +69,7 @@ chmod 744 remove.sh
 touch README.txt
 echo 'Shotcut' > README.txt
 echo 'Idioma=es' >> README.txt
-echo 'Version=20.04.12' >> README.txt
+echo 'Version=20.07.11' >> README.txt
 echo 'Arquitectura=64-bit' >> README.txt
 echo 'Esta es una instalación no invasiva.' >> README.txt
 echo 'Para desinstalar ejecute el fichero remove.sh como root.' >> README.txt
@@ -95,7 +95,7 @@ echo -ne '[=================================>      ](83.30%)\r';sleep 0.2
 echo -ne '[====================================>   ](91.64%)\r';sleep 0.2
 echo -ne '[=======================================>](100.00%)\r';sleep 0.2
 echo -ne '\n'
-rm -rf /usr/local/$DIRINST/shotcut-linux-x86_64-200412.txz
+rm -rf /usr/local/$DIRINST/shotcut-linux-x86_64-200711.txz
 if [ -d /home/*/.config/Meltytech/ ];
 	then
 		rm -rf /home/*/.config/Meltytech/
